8971d1933050071b7463d645db59a630ec06b789
John Lindquist <johnlindquist@computer.local>
2017-12-12 15:48:50 -0700

    vue-class-component

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 @click="onClick">
+    {{message}}
+  </h1>
+</template>
+
+<script>
+import Vue from "vue"
+import Component from "vue-class-component"
+
+@Component({})
+export default class App extends Vue {
+  message = "Hello from class"
+
+  onClick() {
+    this.message = "Goodbye"
+  }
+}
+</script>
--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import "tailwindcss/dist/tailwind.css"
 import Vue from "vue"
+import App from "./App"
 
 new Vue({
   el: "#app",
-  render: h => (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  )
+  render: h => <App />
 })
