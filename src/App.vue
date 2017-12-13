dc8aaf757dd76745b546990b4b56c90c5327f391
John Lindquist <johnlindquist@computer.local>
2017-12-13 09:07:15 -0700

    functional-template

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <Settings>
+    <Layout slot-scope="{header, footer}">
+      <Header slot="header" :header="header"></Header>
+      <div slot="content" class="flex-grow p-4">Amazing Content</div>
+      <Footer slot="footer" :footer="footer"></Footer>
+    </Layout>
+  </Settings>
+</template>
+
+<script>
+import Vue from "vue"
+import { Component, Prop } from "vue-property-decorator"
+import Settings from "./Settings"
+import Layout from "./Layout"
+import { Header, Footer } from "./components"
+
+@Component({
+  components: {
+    Settings,
+    Layout,
+    Header,
+    Footer
+  }
+})
+export default class App extends Vue {
+  @Prop({ default: "Hello from Prop decorator" })
+  message
+
+  onClick() {
+    this.message = "Goodbye"
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,3 @@
+<template functional>
+  <h1 class="bg-blue text-white p-2">{{props.header}}</h1>
+</template>
