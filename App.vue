146cceefd4b211bd46ac6c615d065c4611957e30
John Lindquist <johnlindquist@computer.local>
2017-12-12 17:20:34 -0700

    settings

--- App.vue
+++ App.vue
@@ -1,15 +1,22 @@
 <template>
-  <Layout>
-  </Layout>
+  <Settings>
+    <Layout slot-scope="{header,footer}">
+      <h1 slot="header" class="bg-blue text-white p-2">{{header}}</h1>
+      <div slot="content" class="flex-grow p-3">Amazing content</div>
+      <h2 slot="footer" class="bg-grey-light text-blue p-2 text-center">{{footer}}</h2>
+    </Layout>
+  </Settings>
 </template>
 
 <script>
 import Vue from "vue"
 import { Component, Prop } from "vue-property-decorator"
+import Settings from "./Settings"
 import Layout from "./Layout"
 
 @Component({
   components: {
+    Settings,
     Layout
   }
 })
