0bb11163bb5bdf4a1d9c676a7faf56c41986d18f
John Lindquist <johnlindquist@computer.local>
2017-12-14 00:53:34 -0700

    dynamic-component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
   <Settings>
     <Layout slot-scope="{header, footer}">
       <AwesomeHeader slot="header" :header="header"></AwesomeHeader>
-      <Cats slot="content" :names="names" :num="4"></Cats>
+      <div slot="content" class="flex-grow">
+        <select v-model="selectedComp">
+          <option v-for="comp in comps" :key="comp.name" :value="comp">{{comp.name}}</option>
+        </select>
+        <component :is="selectedComp"></component>
+      </div>
       <AwesomeFooter slot="footer" :footer="footer"></AwesomeFooter>
     </Layout>
   </Settings>
@@ -13,18 +18,36 @@ import Vue from "vue"
 import { Component, Prop } from "vue-property-decorator"
 import Settings from "./Settings"
 import Layout from "./Layout"
-import { Header, Footer, Cats } from "./components"
+import { Header, Footer } from "./components"
+
+const One = {
+  functional: true,
+  name: "One",
+  render: h => <h1 class="bg-red">One</h1>
+}
+
+const Two = {
+  functional: true,
+  name: "Two",
+  render: h => <h1 class="bg-green">Two</h1>
+}
+
+const Three = {
+  functional: true,
+  name: "Three",
+  render: h => <h1 class="bg-purple">Three</h1>
+}
 
 @Component({
   components: {
     Settings,
     Layout,
     AwesomeHeader: Header,
-    AwesomeFooter: Footer,
-    Cats
+    AwesomeFooter: Footer
   }
 })
 export default class App extends Vue {
-  names = ["mindy", "john", "kim", "joel", "ben"]
+  comps = [One, Two, Three]
+  selectedComp = this.comps[0]
 }
 </script>
